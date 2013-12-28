5c1e4130395f3ec8cfc11170e8f5a6fdac84a3c8
randula <randula_codeshock@bitbucket.org>
2013-12-29 00:46:14 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,16 @@
+====================================================================================
+===================== Sample Java Service Wrapper Application ======================
+====================================================================================
+
+Tech Used
+---------
+Build - Maven
+Logger Framework - Log4j/slf4j
+
+Run
+---
+1) Build app - mvn clean install
+2) Run app /target/wrapper-app/bin
+2) run ./startApp console
+
+
---
+++ src/main/java/com/expr/wrapper/app/boot/SystemStarter.java
@@ -0,0 +1,15 @@
+package com.expr.wrapper.app.boot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SystemStarter {
+
+    private static final Logger logger = LoggerFactory.getLogger(SystemStarter.class);
+
+    public static void main(String[] args) {
+        logger.info("================================================================");
+        logger.info("===================== Wrapper App Started ======================");
+        logger.info("================================================================");
+    }
+}
